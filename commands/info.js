9b80a5ee8ad1b8c7e0dd35543c8f3caa6c73c9e2
Christopher Putz <christopher.putz@corgibase.de>
2021-07-04 14:17:14 +0200

    Added information command

---
+++ commands/info.js
@@ -0,0 +1,20 @@
+const package = require('../package.json');
+module.exports =
+{
+    name: "info",
+    usage: "",
+    description: "Gives basic info about " + package.name,
+    example:
+        [
+            ["", "returns basic information about " + package.name]
+        ],
+    execute(message, args)
+    {
+        let infoString = "";
+        infoString += package.name + " v" + package.version + "\n";
+        infoString += package.description + "\n";
+        infoString += "by " + package.author + "\n";
+        infoString += "If you want to contribute, report bugs or suggest features and enhancements, please go to " + package.github;
+        message.channel.send(infoString);
+    },
+};
