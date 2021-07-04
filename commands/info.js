712edb754780a48a92c115ca416a5f3861828ab9
cputz <christopher.putz@corgibase.de>
2021-07-04 14:26:32 +0200

    Fixed info command

--- commands/info.js
+++ commands/info.js
@@ -14,7 +14,7 @@ module.exports =
         infoString += package.name + " v" + package.version + "\n";
         infoString += package.description + "\n";
         infoString += "by " + package.author + "\n";
-        infoString += "If you want to contribute, report bugs or suggest features and enhancements, please go to " + package.github;
+        infoString += "If you want to contribute, report bugs or suggest features and enhancements, please go to " + package.homepage;
         message.channel.send(infoString);
     },
 };
